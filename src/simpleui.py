375f271c65f4ebb674661092b3ca05982f0ead1c
AlexanderCowley <54249135+AlexanderCowley@users.noreply.github.com>
2021-03-17 12:01:03 -0500

    Reformat to PEP8 Standards

--- src/simpleui.py
+++ src/simpleui.py
@@ -7,11 +7,11 @@ def maya_main_window():
     main_window = omui.MQtUtil_mainWindow()
     return wrapInstance(long(main_window), QtWidgets.QDialog)
 
+
 class SimpleUI(QtWidgets.QDialog):
 
     def __init__(self):
         # Passing SimpleUI arguments to super
         # Makes the class python compatible
-
         super(SimpleUI, self).__init__(parent=maya_main_window())
         self.setWindowTitle("A Simple UI")
