f75d9deea83bb00cffb4f1440c5e1e7d04ba430f
AlexanderCowley <54249135+AlexanderCowley@users.noreply.github.com>
2021-04-05 15:56:14 -0500

    Imported QtWidgets, QtCore from Pyside2.
    Pyside2 is going to be used to create the user interface for the scatter tool.
    
    Created a ScatterData class to handle and generate data for the scatter tool.
    
    Created a Scatter UI class to handle the graphical user interface.

---
+++ src/scatter.py
@@ -0,0 +1,13 @@
+from PySide2 import QtWidgets, QtCore
+
+
+class ScatterUI(QtWidgets.QDialog):
+
+    def __init__(self):
+        pass
+
+
+class ScatterData(object):
+
+    def __init__(self):
+        pass
