b9d85c8eebd8372b8161e916a04296f971fc7e7c
AlexanderCowley <54249135+AlexanderCowley@users.noreply.github.com>
2021-04-09 10:31:07 -0500

    Added comments detailing the requirements.

--- src/scatter.py
+++ src/scatter.py
@@ -10,6 +10,22 @@ class ScatterUI(QtWidgets.QDialog):
         pass
 
 
+"Needs:"
+"A SOURCE OBJECT to scatter"
+"A DESTINATION: Can either be an object or vertices"
+"When applied:"
+"INSTANCES must be created of the source object. NOT copies."
+"MOVE the source object onto the DESTINATION VERTICES of the " \
+    "selected object"
+"Randomize the Scale: Have the user determine the min and max of scale"
+"Must be for all three axis"
+"Have it start at 1.0 and increase based on that. 1.20 is 20% scaled"
+"Must be applied to the transform node attributes of the instance"
+"Randomize Rotation Offset: User specifies min max"
+"Angle must be specified in degrees from 0 to 360"
+"Must be applied to the transform node attributes"
+
+
 class ScatterData(object):
 
     def __init__(self):
